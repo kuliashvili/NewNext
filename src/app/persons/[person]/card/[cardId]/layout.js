d519258342710d33824113b94b7fa01894a3b267
giorgi <giorgi.kuliashvili.1@btu.edu.ge>
2024-05-15 20:22:18 +0400

    nested layouts

---
+++ src/app/persons/[person]/card/[cardId]/layout.js
@@ -0,0 +1,13 @@
+function CardLayout( {children} ){
+
+    return (
+        <>
+        <h1>es tag modis layoutidan, qvemot iqneba props children</h1>
+        <div>{children}</div>
+        <h1>chalke gverdebis layout template</h1>
+
+        </>
+    )
+}
+
+export default CardLayout
