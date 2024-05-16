7ef5c64dd198fd487d32b95fd9a290c6434450e7
giorgi <giorgi.kuliashvili.1@btu.edu.ge>
2024-05-16 13:31:25 +0400

    testing state on clientside, show/hide pic

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,20 @@
-function Home(){
-    return <h1>Hello world</h1>
+"use client"; // This is a client component
+import { useState } from "react";
+
+function Home() {
+  const [show, setShow] = useState(true);
+
+  const manipulate = () => {
+    setShow(prevShow => !prevShow)
+    
+  };
+
+  return (
+    <>
+      <button onClick={manipulate}>{show ? "Show pic on website" : "hide pic on website"}</button>
+      {show  && <img id="pic" alt="pic" src="/nowar.png"></img>}
+    </>
+  );
 }
 
-export default Home
+export default Home;
