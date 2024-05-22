1a40a45cb4343a0abbd785fff9576739eff769a3
giorgi <giorgi.kuliashvili.1@btu.edu.ge>
2024-05-22 17:14:52 +0400

    counter

--- src/app/page.js
+++ src/app/page.js
@@ -3,16 +3,22 @@ import { useState } from "react";
 
 function Home() {
   const [show, setShow] = useState(true);
+  const [count, setCount] = useState(0)
 
   const manipulate = () => {
     setShow(prevShow => !prevShow)
     
   };
 
+
   return (
     <>
       <button onClick={manipulate}>{show ? "Show pic on website" : "hide pic on website"}</button>
       {show  && <img id="pic" alt="pic" src="/nowar.png"></img>}
+      <h1>{count}</h1>
+      <button onClick={() => { setCount(count + 1)}} >up</button>
+      <button onClick={() => { setCount(count - 1)}} >down</button>
+      <button onClick={() => { setCount(0)}} >reset</button>
     </>
   );
 }
